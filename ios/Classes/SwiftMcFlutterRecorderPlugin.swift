df0e914908268d883c9a3918996d89522b553f23
lisunfa <sunfa.li@airgram.io>
2023-06-13 20:09:00 +0800

    fix error: NSInternalInconsistencyException Sending a message before the FlutterEngine has been run.

--- ios/Classes/SwiftMcFlutterRecorderPlugin.swift
+++ ios/Classes/SwiftMcFlutterRecorderPlugin.swift
@@ -4,6 +4,7 @@ import UIKit
 public class SwiftMcFlutterRecorderPlugin: NSObject, FlutterPlugin, MCRecorderManagerDelegate {
     
     var channel: FlutterMethodChannel?
+    private var engineActive: Bool = false
     private lazy var _manager: MCRecorderManager = {
         let manager = MCRecorderManager.init()
         manager.delegate = self
@@ -14,6 +15,7 @@ public class SwiftMcFlutterRecorderPlugin: NSObject, FlutterPlugin, MCRecorderMa
         let channel = FlutterMethodChannel(name: "mc_flutter_recorder", binaryMessenger: registrar.messenger())
         let instance = SwiftMcFlutterRecorderPlugin()
         instance.channel = channel
+        instance.engineActive = true
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
     
@@ -51,24 +53,34 @@ public class SwiftMcFlutterRecorderPlugin: NSObject, FlutterPlugin, MCRecorderMa
         }
     }
     
+    public func detachFromEngine(for registrar: FlutterPluginRegistrar) {
+        engineActive = false
+    }
+    
     func updateRecorderState(_ state: MCRecorderState) {
         var arguments = Dictionary<String, String>()
         arguments["state"] = state.rawValue
-        channel?.invokeMethod("updateRecordState", arguments: arguments)
+        if engineActive {
+            channel?.invokeMethod("updateRecordState", arguments: arguments)
+        }
     }
     
     func updateRecorderInfo(_ duration: TimeInterval, _ db: Double) {
         var arguments = Dictionary<String, Any>()
         arguments["duration"] = Int(duration * 1000)
         arguments["db"] = db
-        channel?.invokeMethod("updateRecordInfo", arguments: arguments)
+        if engineActive {
+            channel?.invokeMethod("updateRecordInfo", arguments: arguments)
+        }
     }
     
     func updateRecorderException(_ exception: MCRecorderException, _ message: String) {
         var arguments = Dictionary<String, Any>()
         arguments["errorType"] = exception.rawValue
         arguments["message"] = message
-        channel?.invokeMethod("updateRecordError", arguments: arguments)
+        if engineActive {
+            channel?.invokeMethod("updateRecordError", arguments: arguments)
+        }
     }
 }
 
